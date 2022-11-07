31e2b0e55909f76a758cf1576fce8e3a76c5f3e9
Tom1072 <lamvutungvt@gmail.com>
2022-11-07 14:16:08 -0500

    Compile ncurses

--- src/SDV.c
+++ src/SDV.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <curses.h>
 
-int main(void) {
-	puts("Hello World!!!"); /* prints Hello World!!! */
-	return EXIT_SUCCESS;
+int main(void)
+{
+  //puts("Hello World!!!"); /* prints Hello World!!! */
+  initscr();
+  // cbreak();
+  // noecho();
+  // keypad(stdscr, TRUE);
+
+  printw("Hello World!!!");
+  refresh();
+  getch();
+
+  // int height, width;
+
+  // getmaxyx(stdscr, height, width);
+  // WINDOW * win = newwin(height, width, 0, 0);
+  // wrefresh(win);
+  endwin();
+
+  return EXIT_SUCCESS;
 }
