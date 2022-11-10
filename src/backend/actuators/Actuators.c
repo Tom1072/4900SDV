2871d58ae18450c8618cc19f3b99482397c9bbc5
ThangMinhCao <thangminhcao@gmail.com>
2022-11-10 16:27:24 -0500

    Add actuator changes type

--- src/backend/actuators/Actuators.c
+++ src/backend/actuators/Actuators.c
@@ -1,10 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/neutrino.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+#include <errno.h>
+#include "../includes/commons.h"
+
+#define MAX_STRING_LEN    256
+
 /**
  * Engage the brake level.
  * This sends pulse to Simulator.
  * Constraint: 0 <= level <= 100
 */
 void engageBrake(int level) {
-
+  actuatorChanges_t message;
 }
 
 /**
@@ -13,5 +23,5 @@ void engageBrake(int level) {
  * Constraint: 0 <= level <= 100
 */
 void engageThrottle(int level) {
-
+  actuatorChanges_t message;
 }
--- src/backend/includes/commons.h
+++ src/backend/includes/commons.h
@@ -1,3 +1,12 @@
+#include <sys/dispatch.h>
+
 #define TRUE 1
 #define FALSE 0
+#define BRAKE_ACTUATOR 1
+#define THROTTLE_ACTUATOR 2
 
+typedef union {
+  struct _pulse pulse;
+  int type; // BRAKE_ACTUATOR or THROTTLE_ACTUATOR
+  int level; // 0-100
+} actuatorChanges_t;
